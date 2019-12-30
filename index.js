15670651e02241a924a4bbcf3362107a488ff17e
casternuts <show5960@gmail.com>
2019-12-30 14:37:48 +0900

    On branch master  Changes to be committed: 	new file:   .babelrc

---
+++ index.js
@@ -0,0 +1,9 @@
+import { GraphQLServer } from "graphql-yoga";
+import resolvers from "./graphql/resolvers";
+// ... or using `require()`
+//const { GraphQLServer } = require("graphql-yoga");
+
+const typeDefs = "./graphql/schema.graphql";
+
+const server = new GraphQLServer({ typeDefs, resolvers });
+server.start(() => console.log("Server is running on localhost:4000"));
